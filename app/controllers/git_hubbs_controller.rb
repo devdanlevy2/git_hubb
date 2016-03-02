e41e7662e81afc8451217d9f48dc012e652e3526
devdanlevy <daniel.levy2@iphone.com>
2016-03-01 19:38:28 -0500

    configure controller for ask and show.  Add Gemfile for httparty.

--- app/controllers/git_hubbs_controller.rb
+++ app/controllers/git_hubbs_controller.rb
@@ -1,7 +1,17 @@
 class GitHubbsController < ApplicationController
   def show
+    @information = HTTParty.get('https://api.github.com/users/:username/repos')
+    puts response.body, response.code, response.message, response.headers.inspect
   end
 
   def ask
+
   end
 end
+
+# response = HTTParty.get('http://twitter.com/statuses/public_timeline.json')
+# puts response.body, response.code, response.message, response.headers.inspect
+#
+# response.each do |item|
+#   puts item['user']['screen_name']
+# end
